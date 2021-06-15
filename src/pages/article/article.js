3fcde7c2a99e7987c9aef9bdd7e35976b9efe881
shaoqing <shaoqing@dajiaok.com>
2021-06-15 14:55:29 +0800

    feat: 工具栏点击事件

--- src/pages/article/article.js
+++ src/pages/article/article.js
@@ -1,14 +1,14 @@
 /*
  * @Author: shaoqing
  * @Date: 2021-06-04 13:35:10
- * @LastEditTime: 2021-06-07 18:28:13
+ * @LastEditTime: 2021-06-15 10:17:38
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \react-blog-admin\src\pages\article\article.js
  */
 import React, { Component } from 'react'
 import markdownIt from 'markdown-it'
-import {BoldOutlined, ItalicOutlined, UnderlineOutlined, StrikethroughOutlined} from '@ant-design/icons';
+import { BoldOutlined, ItalicOutlined, StrikethroughOutlined } from '@ant-design/icons'
 import './article.scss'
 import hljs from 'highlight.js'
 import 'highlight.js/styles/github.css'
@@ -17,13 +17,11 @@ const md = new markdownIt({
   highlight: function (str, lang) {
     if (lang && hljs.getLanguage(lang)) {
       try {
-        return '<pre class="hljs"><code>' +
-               hljs.highlight(lang, str, true).value +
-               '</code></pre>';
+        return '<pre class="hljs"><code>' + hljs.highlight(lang, str, true).value + '</code></pre>'
       } catch (__) {}
     }
 
-    return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+    return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>'
   }
 })
 let scrolling = 0
@@ -44,15 +42,15 @@ class Article extends Component {
    */
   handleScroll(type, e) {
     const { scrollHeight, scrollTop, clientHeight } = e.target
-    const ratio  = scrollTop / (scrollHeight - clientHeight)
+    const ratio = scrollTop / (scrollHeight - clientHeight)
 
-    if(Object.is(type, 'edit')) {
-      if(scrolling === 0) scrolling = 1
-      if(scrolling === 2) return
+    if (Object.is(type, 'edit')) {
+      if (scrolling === 0) scrolling = 1
+      if (scrolling === 2) return
       this.evaluateSyncScroll(this.previewRef.current, ratio)
     } else {
-      if(scrolling === 0) scrolling = 2
-      if(scrolling === 1) return
+      if (scrolling === 0) scrolling = 2
+      if (scrolling === 1) return
       this.evaluateSyncScroll(this.editRef.current, ratio)
     }
   }
@@ -64,11 +62,11 @@ class Article extends Component {
   evaluateSyncScroll(target, ratio) {
     const { scrollHeight, clientHeight } = target
     target.scrollTop = (scrollHeight - clientHeight) * ratio
-    
-    if(scrollTimer) clearTimeout(scrollTimer)
+
+    if (scrollTimer) clearTimeout(scrollTimer)
     scrollTimer = setTimeout(() => {
       scrolling = 0
-      clearTimeout(scrollTimer)  
+      clearTimeout(scrollTimer)
     }, 200)
   }
   /**
@@ -81,35 +79,73 @@ class Article extends Component {
     this.setState({ parseMarkdownContent: md.render(value) })
   }
   /**
+   * @description: 处理文字
+   * @param {*}
+   * @return {*}
+   */
+  handleText(text, symbol) {
+    const { selectionStart, selectionEnd, value } = this.editRef.current
+    let newValue =
+      selectionStart === selectionEnd
+        ? value.slice(0, selectionStart) + symbol + text + symbol + value.slice(selectionEnd)
+        : value.slice(0, selectionStart) + symbol + value.slice(selectionStart, selectionEnd) + symbol + value.slice(selectionEnd)
+    this.editRef.current.value = newValue
+    this.setState({ parseMarkdownContent: md.render(newValue) })
+  }
+  /**
    * @description: 字体加粗
    * @param {*} e
    * @return {*}
    */
   handleAddBold = () => {
-    const { selectionStart, selectionEnd, value } = this.editRef.current
-    let newValue = selectionStart === selectionEnd ? value.slice(0, selectionStart) + '**加粗文字**' + value.slice(selectionEnd) :
-    value.slice(0, selectionStart) + '**' + value.slice(selectionStart, selectionEnd) + '**' + value.slice(selectionEnd)
-    this.editRef.current.value = newValue
-    this.setState({ parseMarkdownContent: md.render(newValue) })
+    this.handleText('加粗文字', '**')
   }
+  /**
+   * @description: 字体斜体
+   * @param {*}
+   * @return {*}
+   */
+  handleAddItalic = () => {
+    this.handleText('斜体文字', '*')
+  }
+  /**
+   * @description: 字体删除
+   * @param {*}
+   * @return {*}
+   */
+  handleAddOutlined = () => {
+    this.handleText('删除文字', '~~')
+  }
+  /**
+   * @description: 发布文章
+   * @param {*}
+   * @return {*}
+   */
+  releaseArticle = async () => {}
   render() {
     return (
       <div className="article">
         <div className="article-header">
           <input className="title-input" placeholder="输入文章标题..."></input>
-          <div className="release-btn">发布</div>
+          <div className="release-btn" onClick={this.releaseArticle}>
+            发布
+          </div>
         </div>
         <div className="article-navbar">
           <div className="article-navbar-left">
-            <BoldOutlined className="icon" onClick={this.handleAddBold}/>
-            <ItalicOutlined className="icon"/>
-            <UnderlineOutlined className="icon"/>
-            <StrikethroughOutlined className="icon"/>
+            <BoldOutlined className="icon" onClick={this.handleAddBold} />
+            <ItalicOutlined className="icon" onClick={this.handleAddItalic} />
+            <StrikethroughOutlined className="icon" onClick={this.handleAddOutlined} />
           </div>
         </div>
         <div className="article-content">
-          <textarea className="article-edit" ref={this.editRef} onChange={this.handleTextareaChange} onScroll={(e) => this.handleScroll('edit',e)}></textarea>
-          <div className="article-preview" ref={this.previewRef} dangerouslySetInnerHTML={{ __html: this.state.parseMarkdownContent }} onScroll={(e) => this.handleScroll('preview', e)}></div>
+          <textarea className="article-edit" ref={this.editRef} onChange={this.handleTextareaChange} onScroll={(e) => this.handleScroll('edit', e)}></textarea>
+          <div
+            className="article-preview"
+            ref={this.previewRef}
+            dangerouslySetInnerHTML={{ __html: this.state.parseMarkdownContent }}
+            onScroll={(e) => this.handleScroll('preview', e)}
+          ></div>
         </div>
       </div>
     )
